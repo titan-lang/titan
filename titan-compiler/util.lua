c83dcdb30456122366fa0b65fce42fff474a0440
Hisham Muhammad <hisham@gobolinux.org>
2017-12-20 21:31:17 -0200

    Add utility to_set function

--- titan-compiler/util.lua
+++ titan-compiler/util.lua
@@ -46,6 +46,14 @@ function util.make_visitor(functions)
     end
 end
 
+function util.to_set(array)
+    local set = {}
+    for _, v in ipairs(array) do
+        set[v] = true
+    end
+    return set
+end
+
 --
 -- Functional
 --
